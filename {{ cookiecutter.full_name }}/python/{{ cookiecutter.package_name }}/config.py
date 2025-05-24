3ce1fc855479d4abae74ad34cbecd817e62a6476
Whth <whthyotta@qq.com>
2025-05-24 22:08:24 +0800

    feat: add configuration loader for {{ cookiecutter.project_name }}
    - Import CONFIG from fabricatio_core
    - Create {{ cookiecutter.project_name }}_config instance using CONFIG.load
    - Update {{ cookiecutter.project_name|capitalize }}Config class
    - Add __all__ list to expose {{ cookiecutter.project_name }}_config

--- {{ cookiecutter.full_name }}/python/{{ cookiecutter.package_name }}/config.py
+++ {{ cookiecutter.full_name }}/python/{{ cookiecutter.package_name }}/config.py
@@ -1,6 +1,13 @@
 """Module containing configuration classes for {{ cookiecutter.full_name }}."""
 from dataclasses import dataclass
-
+from fabricatio_core import CONFIG
 @dataclass
 class {{ cookiecutter.project_name|capitalize }}Config:
     """ Configuration for {{ cookiecutter.full_name }}"""
+
+
+
+{{ cookiecutter.project_name }}_config = CONFIG.load("{{ cookiecutter.project_name }}",  {{ cookiecutter.project_name|capitalize }}Config)
+__all__ = [
+    "{{ cookiecutter.project_name }}_config"
+]
