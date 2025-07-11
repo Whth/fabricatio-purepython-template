6118e7f6912cd1415bb4844021e9e04d8013354c
Whth <whthyotta@qq.com>
2025-07-11 16:21:26 +0800

    feat: add models module and update capabilities
    - Add new models module for {{ cookiecutter.project_name }}
    - Update {{ cookiecutter.project_name }} class to inherit from ABC

--- {{ cookiecutter.full_name }}/python/{{ cookiecutter.package_name }}/capabilities/{{ cookiecutter.project_name }}.py
+++ {{ cookiecutter.full_name }}/python/{{ cookiecutter.package_name }}/capabilities/{{ cookiecutter.project_name }}.py
@@ -1,9 +1,9 @@
 """This module contains the capabilities for the {{ cookiecutter.project_name }}."""
+from abc import ABC
 from fabricatio_core.capabilities.usages import UseLLM
 
 
-
-class {{ cookiecutter.project_name | capitalize}}(UseLLM):
+class {{ cookiecutter.project_name | capitalize}}(UseLLM, ABC):
     """This class contains the capabilities for the {{ cookiecutter.project_name }}."""
 
     async def {{cookiecutter.project_name}}(self, **kwargs): ...
